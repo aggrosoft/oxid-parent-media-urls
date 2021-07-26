6b149c7535a6c2f3411c8cbddcd0df801eea24f9
alexander <coding@aggrosoft.de>
2021-07-26 10:58:02 +0200

    initial version

---
+++ Application/Model/ParentMediaUrlsArticle.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Aggrosoft\ParentMediaUrls\Application\Model;
+
+
+class ParentMediaUrlsArticle extends ParentMediaUrlsArticle_parent
+{
+    public function getMediaUrls()
+    {
+        if ($this->_aMediaUrls === null) {
+            $this->_aMediaUrls = oxNew(\OxidEsales\Eshop\Core\Model\ListModel::class);
+            $this->_aMediaUrls->init("oxmediaurl");
+            $this->_aMediaUrls->getBaseObject()->setLanguage($this->getLanguage());
+
+            $tableViewNameGenerator = oxNew(TableViewNameGenerator::class);
+            $sViewName = $tableViewNameGenerator->getViewName("oxmediaurls", $this->getLanguage());
+            $sQ = "select * from {$sViewName} where oxobjectid = :oxobjectid or oxobjectid = :oxparentid";
+            $this->_aMediaUrls->selectString($sQ, [
+                ':oxobjectid' => $this->getId(),
+                ':oxparentid' => $this->oxarticles__oxparentid->value
+            ]);
+        }
+
+        return $this->_aMediaUrls;
+    }
+}
---
+++ metadata.php
@@ -0,0 +1,15 @@
+<?php
+
+$sMetadataVersion = '2.0';
+
+$aModule = array(
+    'id'           => 'agparentmediaurls',
+    'title'        => 'Aggrosoft Parent Media Urls',
+    'description'  => 'Load media urls from parent if variant has none',
+    'thumbnail'    => '',
+    'version'      => '1.0.0',
+    'author'       => 'Aggrosoft GmbH',
+    'extend'      => array(
+        \OxidEsales\Eshop\Application\Model\Article::class => Aggrosoft\ParentMediaUrls\Application\Model\ParentMediaUrlsArticle::class
+    )
+);
